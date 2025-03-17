673040583934de9eb68749463d2acfdb7494a541
Daiane Geraldino <129515871+DaianeGeraldino@users.noreply.github.com>
2025-03-17 16:04:10 -0300

    Update main.c

--- aula11032025/main.c
+++ aula11032025/main.c
@@ -1,50 +1,65 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-double soma(double num1, double num2){
-	return num1 + num2;
+double soma(double num1, double num2) {
+    return num1 + num2;
 }
 
-double subtracao(double num1, double num2){
-	return num1 - num2;
+double subtracao(double num1, double num2) {
+    return num1 - num2;
 }
 
-double divisao(double num1, double num2){
-	return num1/num2;
+double multiplicacao(double num1, double num2) {
+    return num1 * num2;
 }
 
-double multiplicacao(double num1, double num2){
-	return num1*num2;
+double divisao(double num1, double num2) {
+    return num1 / num2;
 }
 
-int main(int argc, char** argv) {
-	char operacao;
-	double a, b;
-	
-	printf("Digite o primeiro numero: ");
-	scanf("%d", &a);
-	
-	printf("Digite o segundo numero: ");
-	scanf("%d", &b);
-	
-	printf("Digite o operador(+, -, /, *): ");
-	scanf("%c", &operacao);
-	
-	switch (operador){
-		case '+':
-			printf(soma(a, b));
-			break;
-		case '-':
-			printf(subtracao(a, b);
-			break;
-		case '/':
-			printf(divisao(a, b));
-			break:
-		case '*':
-			printf(multiplicacao(a, b));
-			break;
-	}
-	
-	return 0;
+int verificaDivisao(double num2) {
+    if (num2 == 0) {
+        printf("Erro: Divisão por zero!\n");
+        return 1;
+    }
+    return 0; 
+}
+
+int main() {  
+    char operacao;
+    double a, b;
+    int continuar = 1;
+
+    while (continuar) {
+        printf("\nDigite o primeiro número: ");
+        scanf("%lf", &a);
+
+        printf("Digite o segundo número: ");
+        scanf("%lf", &b);
+
+        printf("Digite o operador (+, -, /, *): ");
+        scanf(" %c", &operacao);
+
+        switch (operacao) {
+            case '+':
+                printf("Resultado: %.2f\n", soma(a, b));
+                break;
+            case '-':
+                printf("Resultado: %.2f\n", subtracao(a, b));
+                break;
+            case '/':
+                if (!verificaDivisao(b)) printf("Resultado: %.2f\n", divisao(a, b));
+                break;
+            case '*':
+                printf("Resultado: %.2f\n", multiplicacao(a, b));
+                break;
+            default:
+                printf("Operador inválido!\n");
+        }
+
+        printf("Deseja continuar? (1 = Sim, 0 = Não): ");
+        scanf("%d", &continuar);
+    }
+
+    return 0;
 }
