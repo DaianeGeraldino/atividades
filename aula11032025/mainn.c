3bb90ba665cc34f8fdeace354f057e20fd72d657
Daiane Geraldino <129515871+DaianeGeraldino@users.noreply.github.com>
2025-03-21 09:48:16 -0300

    Create mainn.c

---
+++ aula11032025/mainn.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int calFatorial(int fatorial){
+	return fatorial = fatorial * (fatorial - 1);
+}
+
+int main() {
+	int fatorial = rand(fatorial)%14;
+	
+	
+
+    return 0;
+}
